1fd8b063eb86af13f1cdb7ce967fce43e9a9e2c7
gooncalvesgaby <gabrielle.gonalves@outlook.com.br>
2023-04-03 22:05:48 -0300

    rota de criar usuario e +

--- Desktop/api-express/src/controllers/loginController.js
+++ Desktop/api-express/src/controllers/loginController.js
@@ -5,17 +5,25 @@ const loginController = {
     loginPage: (req, res) => {
         res.render('login')
     },
+    
+    cadastrarUsuario: (req, res) => {
+        const novoUsuario = req.body
+        res.send(req.body)
+    },
+
 
     logarUsuario: (req, res) => { 
         //req é a requisição, res é a resposta
+        //aqui estou criando uma variavel que pega as infos que o usuario mandou
         const email = req.body.email
         const senha = req.body.senha
-        
+        // aqui estou tentando encontrar alguem que tenha o mesmo email e senha digitados na pg de login
         const encontrandoUsuario = loginMock.find((user) => {
             if(user.email === email && user.senha === senha) {
                 return user
             }
         }) 
+        // se caso o usuario nao for cadastrado 
         if(encontrandoUsuario === undefined) {
             res.send("Usuario não encontrado")
         } 
@@ -24,7 +32,8 @@ const loginController = {
         
 },
 
-    
-} 
+     
+}       
+
 // aqui eu to exportando o meu objeto
 module.exports= loginController
--- Desktop/api-express/src/routes/index.js
+++ Desktop/api-express/src/routes/index.js
@@ -8,11 +8,13 @@ const loginController = require('../controllers/loginController')
 // aqui eu criei minha variavel pagamentoController que recebe o objeto que eu to exportando da pagamentoController.js
 const pagamentoController = require('../controllers/pagamentoController')
 
+
 router.get('/', homeController.home)
 // aqui eu criei a rota /Login e quando ele acessa a rota ele chama a minha callback
 router.get('/login', loginController.loginPage)
 // aqui eu criei a rota /Pagamento e quando ele acessa a rota ele chama a minha callback
 router.get('/pagamento', pagamentoController.pagamentoPage)
 router.post('/logar', loginController.logarUsuario)
+router.post('/criarconta', loginController.cadastrarUsuario)
 
 module.exports = router
