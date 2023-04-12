240b2eeecc09cd8a654b0ee8317cd8b327e6d865
Gustavo <gustavoalves2039@outlook.com>
2023-04-12 20:14:51 -0300

    add page skate.ejs & ligando rota com a app.js

--- Desktop/api-express/src/app.js
+++ Desktop/api-express/src/app.js
@@ -1,23 +1,28 @@
 const routes = require('./routes/index')
 const path = require("path")
 const express = require('express')
-const methodOverride = require('method-override') // métodos PUT e DELETE
-const app = express()
-// captura na forma de objeto literal tudo o que vem de um formulário
-app.use(express.urlencoded({ extended: false }))
-// converte as informações em formato JSON
+const app = express();
+const ejs = require('ejs');
 app.use(express.json())
-// métodos PUT e DELETE
-app.use(methodOverride('_method'))
+// aqui na linha 8 captura na forma de objeto literal tudo o que vem de um formulário
+app.use(express.urlencoded({ extended: false }))
 
 // instanciando como view engine
 app.set("view engine", "ejs")
+//instaciando Itens EJS - tela Skate.ejs
+app.get('/', function(req, res) {
+  res.render('index', { title: 'Skate - Jetskateshop', buttonLabel: 'Limpar', filterMedidas: 'MEDIDAS', Item1: 'Shapes', Item2: 'Trucks', Item3: 'Rodas', titleItem1: 'Boulevard', valueItem1: 'R$800,00', buttonLabel2: 'Comprar', valueItem2: 'Creature', valueItem3: 'DGK', valueItem4: 'Primitive', 
+  valueItem5: 'Chocolate', valueItem6: 'Diamond', valueItem7: 'Girl', valueItem8: 'Independent', valueItem9: 'Venture', valueItem10: 'Bones',valueItem11: 'Ricta', valueItem12: 'Spit Fire', valueItem13: 'Element'});
+});
 // instanciando pasta views
 app.set("views", path.resolve("src", "views"))
 // liberando acesso a pasta public
+
 app.use(express.static(path.resolve("public")))
 
-
+/**
+ * Rotas
+ */
 app.use(routes)
 
 app.listen(3000, () => {
