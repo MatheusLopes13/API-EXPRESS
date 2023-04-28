8c9fdfb03207f4ee67029bed9ac029d7ad2f065f
Yuri Ardila <yurimerege@hotmail.com>
2023-04-28 09:41:30 -0300

    refactor structure

---
+++ src/routes/index.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const detalsController = require('../controllers/detalsController')
+const router = express.Router()
+const teniscontroller = require('../controllers/tenisController')
+const homeController = require('../controllers/homeController')
+const adminController = require('../controllers/adminController');
+const loginController = require('../controllers/loginController')
+const pagamentoController = require('../controllers/pagamentoController')
+const roupasController = require('../controllers/roupasControler')
+const { body } = require('express-validator')
+
+
+router.get('/',  homeController.home)
+router.get('/tenis', teniscontroller.tenis)
+router.get('/detals/:id', detalsController.getProduct)
+router.get('/search', homeController.search)
+router.get('/admin', adminController.renderizarAdminPage)
+router.post('/product', 
+    body('nameProduct').notEmpty().withMessage('Marca precisa ser preenchido!'),
+    body('valueProduct').notEmpty().withMessage('Valor precisa ser preenchido!'),
+    body('sizeProduct').notEmpty().withMessage('Tamanho precisa ser preenchido!'),
+    body('descriptionProduct').notEmpty().withMessage('Descrição precisa ser preenchido!'),
+    body('colorProduct').notEmpty().withMessage('Cor precisa ser preenchido!'),
+    adminController.addProduct)
+router.get('/login', loginController.renderizarTelaLogin) 
+router.get('/pagamento', pagamentoController.pagamentoPage)
+router.get('/roupas', roupasController.roupasPage)
+router.post('/logar', loginController.logarUsuario)
+
+module.exports = router
+
+
+
