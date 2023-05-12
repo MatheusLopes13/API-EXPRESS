29cfa83683a86b271814322feb26f0c28641c004
MatheusLopes13 <matheuuslopes13@gmail.com>
2023-05-11 21:49:23 -0300

    pagina detals/search

--- src/controllers/detalsController.js
+++ src/controllers/detalsController.js
@@ -1,13 +1,13 @@
-const allProducts = require('../database/allProduct')
+
 const carrinhoProdutos = require('../database/carrinhoProdutos.json')
 const { Product } = require('../models')
+const {User} = require('../models')
 
 const detalsController  = {
     getProduct: async (req, res) => {
-         let id = req.params.id
+         const { id }= req.params
          
          const product_ = await Product.findOne({ where: { id } })
-
          
         res.render('detals' , {product: product_,  carrinhoProdutos: carrinhoProdutos})
     }
