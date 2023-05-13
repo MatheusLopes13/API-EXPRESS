c01b5c3b9674249cf1766943391733ce757a9324
FelipeBattistotti <felipe.battistotti@gmail.com>
2023-05-13 15:09:00 -0300

    Listagem dos Produtos Categoria Roupas - PROFESSOR

--- src/controllers/roupasControler.js
+++ src/controllers/roupasControler.js
@@ -1,9 +1,30 @@
-const roupasProduct = require('../database/productRoupas.json')
+// const roupasProduct = require('../database/productRoupas.json')
 const carrinhoProdutos = require('../database/carrinhoProdutos.json')
+const { Product, ProductType } = require('../models')
 
 const roupasController = {
-    roupasPage: (req, res) => {
-        res.render('roupas', {roupasProduct: roupasProduct, carrinhoProdutos: carrinhoProdutos })
+  roupasPage: async (req, res) => {
+    // res.render('roupas', {roupasProduct: roupasProduct, carrinhoProdutos: carrinhoProdutos })
+
+    try {
+      const roupasProduct = await Product.findAll({
+        include: {
+          model: ProductType,
+          as: 'productType',
+          required: true, // aplica INNER JOIN
+          where: {
+            id: 2
+          }
+        }
+      })
+
+      res.render('roupas', {
+        roupasProduct: roupasProduct,
+        carrinhoProdutos: carrinhoProdutos
+      })
+    } catch (error) {
+      res.status(400).json({ error })
     }
+  }
 }
 module.exports = roupasController
--- src/models/Product.js
+++ src/models/Product.js
@@ -1,29 +1,31 @@
-const { INTEGER } = require("sequelize")
-
 module.exports = (sequelize, DataType) => {
-    const Product = sequelize.define('Product', {
-        id: {
-            type: DataType.INTEGER,
-            primaryKey: true,
-            autoIncrement: true
-        },
-        nome: DataType.STRING(30),
-        valor: DataType.DECIMAL(10, 2),
-        cor: DataType.STRING(20),
-        descricao: DataType.STRING(100),
-        id_categoria: DataType.INTEGER,
-        imagem_1: DataType.STRING(),
-        imagem_2: DataType.STRING(),
-        imagem_3: DataType.STRING(),
-        codigo_produto: DataType.STRING(45)
+  const Product = sequelize.define('Product', {
+    id: {
+      type: DataType.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    nome: DataType.STRING(30),
+    valor: DataType.DECIMAL(10, 2),
+    cor: DataType.STRING(20),
+    descricao: DataType.STRING(100),
+    id_categoria: DataType.INTEGER,
+    imagem_1: DataType.STRING(),
+    imagem_2: DataType.STRING(),
+    imagem_3: DataType.STRING(),
+    codigo_produto: DataType.STRING(45)
 
-    }, {
-        timestamps: false,
-        tableName: 'produto'
-    }
+  }, {
+    timestamps: false,
+    tableName: 'produto'
+  })
 
+  Product.associate = (modelsList) => {
+    Product.belongsTo(modelsList.ProductType, {
+      foreignKey: 'id_categoria',
+      as: 'productType'
+    })
+  }
 
-    )
-
-    return Product
+  return Product
 }
